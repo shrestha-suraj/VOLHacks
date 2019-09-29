182f13b8ebb03a3b44a2bb9cfca9b654c1794140
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-09-29 08:10:03 -0500

    UI Fixed

--- js/index.js
+++ js/index.js
@@ -12,36 +12,29 @@ function searchStock(e) {
         function (checkObj) {
             stock_symbol = checkObj['Meta Data']['2. Symbol'];
             stock_value = checkObj['Time Series (1min)']['2019-09-27 14:21:00']['4. close'];
-            var count=0;
-            (function($){
-                jQuery.fn.checkEmpty = function() {
-                   return !$.trim(this.html()).length;
+            var count = 0;
+            (function ($) {
+                jQuery.fn.checkEmpty = function () {
+                    return !$.trim(this.html()).length;
                 };
             }(jQuery));
-            for (var i=1;i<=3;i++){
-                if($(`#box${i}`).checkEmpty()){
+            for (var i = 1; i <= 3; i++) {
+                if ($(`#box${i}`).checkEmpty()) {
                     var card = document.getElementById(`box${i}`);
                     var trigger = document.getElementById(`row${i}`);
-                    card.innerHTML="<span><h2>"+stock_symbol+"</h2><br/>"+"<h3>"+stock_value+"</h3></span>";
-                    trigger.innerHTML="<h2>"+stock_symbol+"</h2><span><input type='number' placeholder='Trigger Price' id='trigger-price'"+i+"> <button id='save-trigger'"+i+">Save</button></span>";
-                    break;    
-               }else{
-                   if(i===3){
-                       alert("Please Clear A Spot First");
-                   }
-               }
-                
+                    card.innerHTML = "<span><h2>" + stock_symbol + "</h2><br/>" + "<h3>" + stock_value + "</h3></span>";
+                    trigger.innerHTML = "<h2>" + stock_symbol + `</h2><span><input type='number' placeholder='Trigger Price' id='trigger-price${i}'> <button id='save-trigger${i}' onclick='changeDesign(${i})'>Add</button></span>`;
+                    break;
+                } else {
+                    if (i === 3) {
+                        alert("Please Clear A Spot First");
+                        break;
+                    }
+                }
+
                 // console.log(card);
             }
         }
     )
-
 }
-
-var addButton=document.getElementById("save-trigger1");
-addButton.addEventListener("click",function(){
-    var amount=document.getElementById("trigger-price1").value;
-});
-
-
 
